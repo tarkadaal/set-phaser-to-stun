aebfa1fba7515905ab286cb92734d1740fd64f11
Dan Jackson <danjacksonis@googlemail.com>
2024-02-24 14:04:23 +0100

    Moved playfield code into its own module, and added tests.

--- src/level.js
+++ src/level.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser/dist/phaser.min.js'
+import PlayField from './play_field.js'
 import DevTile from 'Assets/textures/dev-tile.png'
 
 const DEFAULT_TILE = 'dev-tile'
@@ -6,20 +7,15 @@ const DEFAULT_TILE = 'dev-tile'
 export default class Level extends Phaser.Scene {
   preload () {
     this.load.image(DEFAULT_TILE, DevTile)
-    this.GRID_HEIGHT = 20
-    this.GRID_WIDTH = 20
-    this.TILE_HEIGHT = 24
-    this.TILE_WIDTH = 24
+    this._playField = new PlayField(20, 20, { x: 40, y: 60 }, 24, 24)
   }
 
   create () {
     this.add.text(20, 20, 'This is where the magic will happen, baby')
 
-    for (let gridX = 0; gridX < this.GRID_WIDTH; gridX++) {
-      for (let gridY = 0; gridY < this.GRID_HEIGHT; gridY++) {
-        const tile = this.add.image(gridX * this.TILE_WIDTH, gridY * this.TILE_HEIGHT, DEFAULT_TILE)
-        tile.setOrigin(0, 0)
-      }
+    for (const coordinate of this._playField.getTileCoordinates()) {
+      const tile = this.add.image(coordinate.x, coordinate.y, DEFAULT_TILE)
+      tile.setOrigin(0, 0)
     }
   }
 }
